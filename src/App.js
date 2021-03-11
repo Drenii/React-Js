098836f5ef16e8566db54cdde07aadb62dc6fb82
Drenii <dreni107@hotmail.com>
2021-03-11 19:22:47 +0100

    ReactJS

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {Switch,Route,NavLink,Redirect} from 'react-router-dom';
+import PokemonList from './containers/PokemonList';
+import Pokemon from './containers/Pokemon';
+
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <NavLink to="/">Dren Blakaj </NavLink>
+      </nav>
+      <Switch>
+        <Route path={'/'} exact component={PokemonList}/>
+        <Route path={'/pokemon/:pokemon'} exact component={Pokemon}/>
+        <Redirect to={'/'}/>
+      </Switch>
+      
+
+    </div>
+  );
+}
+
+export default App;
